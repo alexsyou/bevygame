da909ea4b61924d38247d8602a0898b686ae7860
alexsyou <alexandersyou@gmail.com>
2024-06-15 00:11:56 -0400

    feat: added score, sounds, font

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
+use bevy::audio::Volume;
 use bevy::prelude::*;
+use bevy::window::WindowResolution;
+use rand::Rng;
 use std::f32::consts::PI;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(600., 600.).with_scale_factor_override(1.0),
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::rgb(0.7, 0.7, 0.7)))
+        .insert_resource(Score { value: 0 })
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, move_player)
+        .add_systems(FixedUpdate, (player_move, score_display))
+        .add_systems(PostUpdate, item_interact)
         .run();
+}
+
+#[derive(Resource)]
+struct Score {
+    value: u32,
 }
 
 #[derive(Component)]
 struct Player;
 
-#[derive(Component, Debug)]
+#[derive(Default, Component, Debug)]
 enum Direction {
     North,
     South,
+    #[default]
     East,
     West,
     NorthEast,
@@ -25,20 +43,50 @@ enum Direction {
     Still,
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+#[derive(Component)]
+struct Item;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, score: Res<Score>) {
+    let font = asset_server.load("fonts/NotJamChunky8.ttf");
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size: 15.0,
+        color: Color::ANTIQUE_WHITE,
+    };
     commands.spawn(Camera2dBundle::default());
+    commands.spawn((Text2dBundle {
+        text: Text::from_sections([
+            TextSection::new("Where fish?\n\n", text_style.clone()),
+            TextSection::new(format!("Score: {}", score.value), text_style.clone()),
+        ]),
+        transform: Transform::from_xyz(-200., 200., 0.),
+        ..default()
+    },));
     commands.spawn((
         Player,
         SpriteBundle {
-            texture: asset_server.load("penguin.png"),
-            transform: Transform::from_xyz(0., 0., 0.),
+            texture: asset_server.load("penguin2.png"),
+            transform: Transform::from_xyz(0., 0., 1.),
             ..default()
         },
         Direction::East,
     ));
+    commands.spawn((
+        Item,
+        SpriteBundle {
+            texture: asset_server.load("fish.png"),
+            transform: Transform::from_xyz(100., 100., 0.),
+            ..default()
+        },
+    ));
 }
 
-fn move_player(
+fn score_display(score: Res<Score>, mut text: Query<&mut Text>) {
+    let mut text = text.single_mut();
+    text.sections[1].value = format!("Score: {}", score.value);
+}
+
+fn player_move(
     mut players: Query<(&mut Transform, &mut Direction), With<Player>>,
     keys: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
@@ -58,7 +106,7 @@ fn move_player(
             dir.x -= 1.;
         }
 
-        let move_speed = 30.;
+        let move_speed = 100.;
         let move_delta = dir * move_speed * time.delta_seconds();
         transform.translation += move_delta.extend(0.);
 
@@ -81,6 +129,45 @@ fn move_player(
             _ => Direction::Still,
         };
 
-        println!("{:?}", direction);
+        // println!("{:?}", direction);
+    }
+}
+
+fn item_interact(
+    mut commands: Commands,
+    items: Query<(Entity, &Transform), With<Item>>,
+    players: Query<&Transform, With<Player>>,
+    asset_server: Res<AssetServer>,
+    mut score: ResMut<Score>,
+) {
+    let mut rng = rand::thread_rng();
+    let player_transform = players.single();
+
+    for (item, transform) in &items {
+        // println!(
+        //     "player @ {:?}, item @ {:?}",
+        //     player_transform.translation, transform.translation
+        // );
+        if player_transform
+            .translation
+            .abs_diff_eq(transform.translation, 10.)
+        {
+            commands.entity(item).despawn();
+            let x = (rng.gen::<f32>() - 0.5) * 400.;
+            let y = (rng.gen::<f32>() - 0.5) * 400.;
+            commands.spawn((
+                Item,
+                SpriteBundle {
+                    texture: asset_server.load("fish.png"),
+                    transform: Transform::from_xyz(x, y, 0.),
+                    ..default()
+                },
+            ));
+            commands.spawn(AudioBundle {
+                source: asset_server.load("sounds/bubble.ogg"),
+                settings: PlaybackSettings::DESPAWN.with_volume(Volume::new(1.)),
+            });
+            score.value = score.value + 1;
+        }
     }
 }
